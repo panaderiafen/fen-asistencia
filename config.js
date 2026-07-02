66355f106073520c31dc2df4722290f1424cbd4a
panaderiafen <panaderiafen@gmail.com>
2026-07-01 20:05:37 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const FEN_CONFIG = {
+  GOOGLE_CLIENT_ID: '665391435633-67kknu3rnptam8tob8ujs7o6ggi6hpm1.apps.googleusercontent.com',
+  SPREADSHEET_ID:   '1pJFTUSL47jIKjXmBTiG1xrGNTm2gKPe_hdPCgfiqhBU',
+  API_KEY:          'AIzaSyDvT84zDSX9HyHjITxoqdVUj0QvH3J59DE',
+  ADMIN_EMAIL:      'emmanuel.vepal@gmail.com',
+  BHE_DESCUENTO:    0.1525,
+  FERIADO_MULTIPLICADOR: 1.5,
+};
