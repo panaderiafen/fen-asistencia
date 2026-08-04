d44d4779d60e77dc205f13cbbea55f3ac0ab764a
panaderiafen <panaderiafen@gmail.com>
2026-08-04 15:49:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const FEN_CONFIG = {
   SPREADSHEET_ID:   '1pJFTUSL47jIKjXmBTiG1xrGNTm2gKPe_hdPCgfiqhBU',
   API_KEY:          'AIzaSyDvT84zDSX9HyHjITxoqdVUj0QvH3J59DE',
   ADMIN_EMAIL:      'emmanuel.vepal@gmail.com',
-  ADMIN_PASSWORD:   'CAMBIA_ESTA_CLAVE',
+  ADMIN_PASSWORD:   'fenemma-admin',
   APPS_SCRIPT_URL:  'https://script.google.com/macros/s/AKfycbxiePnuf-lVZYeSipFgRLjFZpukCF78j4Bl6aQsQt6bkDOznOZXXsIi9YntXM0b101F/exec',
   BHE_DESCUENTO:    0.1525,
   FERIADO_MULTIPLICADOR: 1.5,
