9e49a065422b9ec17e3add5b812d54e099724264
panaderiafen <panaderiafen@gmail.com>
2026-08-04 15:45:46 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const FEN_CONFIG = {
-  GOOGLE_CLIENT_ID: '665391435633-67kknu3rnptam8tob8ujs7o6ggi6hpm1.apps.googleusercontent.com',
   SPREADSHEET_ID:   '1pJFTUSL47jIKjXmBTiG1xrGNTm2gKPe_hdPCgfiqhBU',
   API_KEY:          'AIzaSyDvT84zDSX9HyHjITxoqdVUj0QvH3J59DE',
   ADMIN_EMAIL:      'emmanuel.vepal@gmail.com',
+  ADMIN_PASSWORD:   'CAMBIA_ESTA_CLAVE',
   APPS_SCRIPT_URL:  'https://script.google.com/macros/s/AKfycbxiePnuf-lVZYeSipFgRLjFZpukCF78j4Bl6aQsQt6bkDOznOZXXsIi9YntXM0b101F/exec',
   BHE_DESCUENTO:    0.1525,
   FERIADO_MULTIPLICADOR: 1.5,
